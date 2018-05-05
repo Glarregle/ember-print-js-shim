0598f877ce68fd75682718d2b6cc11067b2da0e0
Gabriela Larregle <gabilarreglemail@gmail.com>
2018-05-05 00:33:38 -0300

    import dependencies

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-print-js-shim'
+  name: 'ember-print-js-shim',
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.importDependencies();
+  },
+
+  importDependencies() {
+    this.import(
+      {
+        development: 'node_modules/print-js/dist/print.js',
+        production: 'node_modules/print-js/dist/print.min.js'
+      }
+    );
+    this.import('node_modules/print-js/dist/print.min.css');
+    this.import('vendor/print-js-shim.js');
+
+  }
+
 };
